3ed010526da9dcb962fc3e86471f175416ccb364
LautaroNavarro <navarro_lautaro@hotmail.com>
2018-04-27 18:42:05 -0300

    Se agrega un carrucel al index y sus recuersos

--- Vista/index.php
+++ Vista/index.php
@@ -41,7 +41,35 @@
 		<input type="submit" class="btn btn-success">
 	</form>
 
-
+	<!-- Carrucel de fotos -->
+	<div id="carouselExampleIndicators" class="carousel slide" data-ride="carousel">
+		<h1>Algunas fotos mias</h1>
+		<ol class="carousel-indicators">
+			<li data-target="#carouselExampleIndicators" data-slide-to="0" class="active"></li>
+			<li data-target="#carouselExampleIndicators" data-slide-to="1"></li>
+			<li data-target="#carouselExampleIndicators" data-slide-to="2"></li>
+		</ol>
+		<div class="carousel-inner" role="listbox">
+			<div class="carousel-item active">
+				<img class="d-block img-fluid" src="resources/imagen_bot_tres.png" alt="Bot en EventBrite">
+			</div>
+			<div class="carousel-item">
+				<img class="d-block img-fluid" src="resources/imagen_bot_dos.png" alt="Sesión de fotos bot dos">
+			</div>
+			<div class="carousel-item">
+				<img class="d-block img-fluid" src="resources/imagen_bot_uno.png" alt="Sesion de fotos bot 4">
+			</div>
+		</div>
+		<a class="carousel-control-prev" href="#carouselExampleIndicators" role="button" data-slide="prev">
+			<span class="carousel-control-prev-icon" aria-hidden="true"></span>
+			<span class="sr-only">Previous</span>
+		</a>
+		<a class="carousel-control-next" href="#carouselExampleIndicators" role="button" data-slide="next">
+			<span class="carousel-control-next-icon" aria-hidden="true"></span>
+			<span class="sr-only">Next</span>
+		</a>
+	</div>
+	<!-- Fin del carrucel -->
 
 	<br>
 </main>
